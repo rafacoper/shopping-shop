3b3247b3f21ea09ad201025b730db98aaef87058
rafa-opb <rafaelbarbosa.tecnologia@outlook.com>
2023-04-05 20:39:19 -0300

    wk3-auth-skeleton implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
